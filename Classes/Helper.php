9c176fb043bc306ebff4565e7b4a18a4ac866c23
Andreas Wolf <andreas.wolf@ikt-werk.de>
2011-08-12 21:51:15 +0200

    [TASK] Introduce Dublin Core (dc) prefix/namespace

--- Classes/Helper.php
+++ Classes/Helper.php
@@ -36,6 +36,7 @@
  */
 class Tx_RdfExport_Helper {
 	protected static $prefixes = array(
+		'dc' => 'http://purl.org/dc/elements/1.1/',
 		'dcterms' => 'http://purl.org/dc/terms/',
 		'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
 		'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -34,6 +34,7 @@
  */
 abstract class Tx_RdfExport_TestCase extends Tx_Phpunit_TestCase {
 	protected $prefixes = array(
+		'dc' => 'http://purl.org/dc/elements/1.1/',
 		'dcterms' => 'http://purl.org/dc/terms/',
 		'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
 		'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
